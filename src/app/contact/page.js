cb76141f8a375b220d261f5821059032b129eb16
anuj <anuj7870040353@gmail.com>
2023-05-31 17:59:44 +0530

    updated contact page for typo

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -11,7 +11,7 @@ const Contact = () => {
 
         <section className={styles.contact_section}>
           <h2>
-            We'd love to hear <span> from you </span>
+            We&apos;d love to hear <span> from you </span>
           </h2>
 
           <ContactForm />
